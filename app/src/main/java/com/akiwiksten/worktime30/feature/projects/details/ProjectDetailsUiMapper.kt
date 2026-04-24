8b1bdd8295b3651d557a6b836482de580708903d
Aki Wiksten <akiwiksten@gmail.com>
2026-04-24 13:21:19 +0300

    Fix @Suppress(LongMethod) fun applyEntitiesToState()

--- app/src/main/java/com/akiwiksten/worktime30/feature/projects/details/ProjectDetailsUiMapper.kt
+++ app/src/main/java/com/akiwiksten/worktime30/feature/projects/details/ProjectDetailsUiMapper.kt
@@ -3,77 +3,74 @@ package com.akiwiksten.worktime30.feature.projects.details
 import com.akiwiksten.worktime30.core.ZERO_TIME
 
 object ProjectDetailsUiMapper {
-    @Suppress("LongMethod")
     fun applyEntitiesToState(
         baseState: ProjectDetailsUiState.Success,
         projectDetails: ProjectDetailsState?,
         workStats: WorkStatsState?
     ): ProjectDetailsUiState.Success {
-        var state = baseState.data
-        state = if (workStats != null) {
-            state.copy(
-                workStats = WorkStatsState(
-                    dailyWorkTime = workStats.dailyWorkTime.ifEmpty { "07:30" },
-                    lunchTime = workStats.lunchTime.ifEmpty { ZERO_TIME },
-                    flexTimeTotal = workStats.flexTimeTotal.ifEmpty { ZERO_TIME }
-                )
-            )
-        } else {
-            state.copy(
-                workStats = WorkStatsState(
-                    dailyWorkTime = "07:30",
-                    lunchTime = ZERO_TIME,
-                    flexTimeTotal = ZERO_TIME
-                )
-            )
-        }
+        val stateWithWorkStats = baseState.data.copy(workStats = normalizedWorkStats(workStats = workStats))
+        val mappedData = projectDetails?.let {
+            applyProjectDetails(state = stateWithWorkStats, projectDetails = it)
+        } ?: applyEmptyDayDefaults(state = stateWithWorkStats)
 
-        return if (projectDetails != null) {
-            val normalizedStartTime = projectDetails.startTime.ifEmpty { ZERO_TIME }
-            val normalizedEndTime = projectDetails.endTime.ifEmpty { ZERO_TIME }
-            val normalizedProjectTime = normalizeProjectTimeOnOpen(
-                startTime = normalizedStartTime,
-                endTime = normalizedEndTime,
-                projectTime = projectDetails.projectTime.ifEmpty { ZERO_TIME }
-            )
-            baseState.copy(
-                data = state.copy(
-                    date = projectDetails.date,
-                    projectName = projectDetails.projectName,
-                    startTime = normalizedStartTime,
-                    endTime = normalizedEndTime,
-                    lunchStart = projectDetails.lunchStart.ifEmpty { ZERO_TIME },
-                    lunchEnd = projectDetails.lunchEnd.ifEmpty { ZERO_TIME },
-                    breakStart = projectDetails.breakStart.ifEmpty { ZERO_TIME },
-                    breakEnd = projectDetails.breakEnd.ifEmpty { ZERO_TIME },
-                    projectTime = normalizedProjectTime,
-                    flexTimeToday = projectDetails.flexTimeToday.ifEmpty { ZERO_TIME },
-                    oldFlexTimeToday = projectDetails.flexTimeToday.ifEmpty { ZERO_TIME },
-                    isNewDay = isNewDay(
-                        projectDetails.copy(
-                            startTime = normalizedStartTime,
-                            endTime = normalizedEndTime,
-                            projectTime = normalizedProjectTime
-                        )
-                    )
-                )
-            )
-        } else {
-            baseState.copy(
-                data = state.copy(
-                    isNewDay = true,
-                    startTime = ZERO_TIME,
-                    endTime = ZERO_TIME,
-                    lunchStart = ZERO_TIME,
-                    lunchEnd = ZERO_TIME,
-                    breakStart = ZERO_TIME,
-                    breakEnd = ZERO_TIME,
-                    projectTime = ZERO_TIME,
-                    flexTimeToday = ZERO_TIME,
-                    oldFlexTimeToday = ZERO_TIME
-                )
-            )
-        }
+        return baseState.copy(data = mappedData)
+    }
+
+    private fun normalizedWorkStats(workStats: WorkStatsState?): WorkStatsState {
+        val data = workStats ?: WorkStatsState()
+        return WorkStatsState(
+            dailyWorkTime = data.dailyWorkTime.ifEmpty { "07:30" },
+            lunchTime = data.lunchTime.ifEmpty { ZERO_TIME },
+            flexTimeTotal = data.flexTimeTotal.ifEmpty { ZERO_TIME }
+        )
+    }
+
+    private fun applyProjectDetails(
+        state: ProjectDetailsState,
+        projectDetails: ProjectDetailsState
+    ): ProjectDetailsState {
+        val normalizedStartTime = projectDetails.startTime.ifEmpty { ZERO_TIME }
+        val normalizedEndTime = projectDetails.endTime.ifEmpty { ZERO_TIME }
+        val normalizedProjectTime = normalizeProjectTimeOnOpen(
+            startTime = normalizedStartTime,
+            endTime = normalizedEndTime,
+            projectTime = projectDetails.projectTime.ifEmpty { ZERO_TIME }
+        )
+        val normalizedProjectDetails = projectDetails.copy(
+            startTime = normalizedStartTime,
+            endTime = normalizedEndTime,
+            projectTime = normalizedProjectTime
+        )
+
+        return state.copy(
+            date = projectDetails.date,
+            projectName = projectDetails.projectName,
+            startTime = normalizedStartTime,
+            endTime = normalizedEndTime,
+            lunchStart = projectDetails.lunchStart.ifEmpty { ZERO_TIME },
+            lunchEnd = projectDetails.lunchEnd.ifEmpty { ZERO_TIME },
+            breakStart = projectDetails.breakStart.ifEmpty { ZERO_TIME },
+            breakEnd = projectDetails.breakEnd.ifEmpty { ZERO_TIME },
+            projectTime = normalizedProjectTime,
+            flexTimeToday = projectDetails.flexTimeToday.ifEmpty { ZERO_TIME },
+            oldFlexTimeToday = projectDetails.flexTimeToday.ifEmpty { ZERO_TIME },
+            isNewDay = isNewDay(normalizedProjectDetails)
+        )
+    }
+
+    private fun applyEmptyDayDefaults(state: ProjectDetailsState): ProjectDetailsState {
+        return state.copy(
+            isNewDay = true,
+            startTime = ZERO_TIME,
+            endTime = ZERO_TIME,
+            lunchStart = ZERO_TIME,
+            lunchEnd = ZERO_TIME,
+            breakStart = ZERO_TIME,
+            breakEnd = ZERO_TIME,
+            projectTime = ZERO_TIME,
+            flexTimeToday = ZERO_TIME,
+            oldFlexTimeToday = ZERO_TIME
+        )
     }
 
     fun normalizeProjectTimeOnOpen(startTime: String, endTime: String, projectTime: String): String {
